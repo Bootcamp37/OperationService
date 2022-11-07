59c66b9c2a34b4d6cbc65d88d0706be7e4044c74
Pedro <pdiazstam@gmail.com>
2022-11-07 12:37:15 -0500

    Fixed

--- src/main/java/com/nttdata/bootcamp/OperationService/domain/OperationService.java
+++ src/main/java/com/nttdata/bootcamp/OperationService/domain/OperationService.java
@@ -17,6 +17,7 @@ import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 @Slf4j
 @Service
@@ -57,45 +58,53 @@ public class OperationService implements IOperationService {
                                                 .filter(x -> x.getCustomerPassiveProductId().equals(e.getCustomerPassiveProductId()))
                                                 .count()
                                                 .flatMap(count -> {
-                                                    log.info(" 1 count            ===> " + count.toString());
-                                                    log.info(" 1 MovementLimit    ===> " + customerPassiveProductResponse.getMovementLimit());
-                                                    // YA NO HAY MOVIMIENTOS
+                                                  log.info(" 1 count            ===> " + count.toString());
+                                                  // log.info(" 1 MovementLimit    ===> " + customerPassiveProductResponse.getMovementLimit());
+                                                  // YA NO HAY MOVIMIENTOS
+                                                  if(!Objects.isNull(customerPassiveProductResponse.getMovementLimit())) {
                                                     if (count.compareTo(Long.valueOf(customerPassiveProductResponse.getMovementLimit())) >= 0) {
-                                                        return Mono.error(RuntimeException::new);
+                                                      return Mono.error(RuntimeException::new);
                                                     }
-                                                    Double commission = 0.0;
-                                                    log.info(" 2 count            ===> " + count.toString());
-                                                    log.info(" 2 MaxMovementFree  ===> " + customerPassiveProductResponse.getMaxMovementFree());
-                                                    // NO ME QUEDAN MOVIMIENTOS LIBRES
-                                                    if (count.compareTo(Long.valueOf(customerPassiveProductResponse.getMaxMovementFree())) >= 0) {
-                                                        commission = customerPassiveProductResponse.getCommission();
-                                                    }
-                                                    log.info(" 3 Commission       ===> " + commission.toString());
-                                                    // RETIRNO
-                                                    // NO TENGO SALDO
-                                                    if (e.getOperationType().equals(OperationType.WITHDRAWAL) && customerPassiveProductResponse.getAmount() < (e.getAmount() + commission)) {
-                                                        // Retorna error
-                                                        return Mono.error(RuntimeException::new);
-                                                    }
-                                                    // HACER LA TRANSACCION
-                                                    CustomerPassiveProductRequest update = new CustomerPassiveProductRequest();
-                                                    BeanUtils.copyProperties(customerPassiveProductResponse, update);
-                                                    log.info(" BD      ====> " + customerPassiveProductResponse.toString());
-                                                    log.info(" Update  ====> " + update.toString());
-                                                    Double amountUpdate = this.updateAmount(customerPassiveProductResponse.getAmount(), commission, e.getOperationType(), e.getAmount());
-                                                    update.setAmount(amountUpdate);
-                                                    // SETEAR FECHA
-                                                    e.setOperationDate(getDate());
-                                                    // SETEAR COMISION EN LA OPERACIÓN
-                                                    e.setCommission(commission);
-                                                    // Actualizar saldo
-                                                    return customerProductRepository.update(update, e.getCustomerPassiveProductId())
-                                                            // Guardar operacion
-                                                            .flatMap(p -> Mono.just(e))
-                                                            .map(mapper::toEntity)
-                                                            .flatMap(repository::save)
-                                                            .map(mapper::toResponse)
-                                                            .switchIfEmpty(Mono.error(RuntimeException::new));
+                                                  }
+
+                                                  Double commission = 0.0;
+                                                  // log.info(" 2.0 Here  ===> " + Objects.isNull(customerPassiveProductResponse.getMaxMovementFree()));
+                                                  log.info(" 2 count            ===> " + count.toString());
+                                                  log.info(" 2 MaxMovementFree  ===> " + customerPassiveProductResponse.getMaxMovementFree());
+                                                  // NO ME QUEDAN MOVIMIENTOS LIBRES
+                                                  if(Objects.isNull(customerPassiveProductResponse.getMaxMovementFree())) {
+                                                    commission = 0.0;
+                                                    log.info(" 2.1 Here  ===> " + commission.toString());
+                                                  }
+                                                  else if (count.compareTo(Long.valueOf(customerPassiveProductResponse.getMaxMovementFree())) >= 0) {
+                                                    commission = customerPassiveProductResponse.getCommission();
+                                                  }
+                                                  log.info(" 3 Commission       ===> " + commission.toString());
+                                                  // RETIRNO
+                                                  // NO TENGO SALDO
+                                                  if (e.getOperationType().equals(OperationType.WITHDRAWAL) && customerPassiveProductResponse.getAmount() < (e.getAmount() + commission)) {
+                                                    // Retorna error
+                                                    return Mono.error(RuntimeException::new);
+                                                  }
+                                                  // HACER LA TRANSACCION
+                                                  CustomerPassiveProductRequest update = new CustomerPassiveProductRequest();
+                                                  BeanUtils.copyProperties(customerPassiveProductResponse, update);
+                                                  log.info(" BD      ====> " + customerPassiveProductResponse.toString());
+                                                  log.info(" Update  ====> " + update.toString());
+                                                  Double amountUpdate = this.updateAmount(customerPassiveProductResponse.getAmount(), commission, e.getOperationType(), e.getAmount());
+                                                  update.setAmount(amountUpdate);
+                                                  // SETEAR FECHA
+                                                  e.setOperationDate(getDate());
+                                                  // SETEAR COMISION EN LA OPERACIÓN
+                                                  e.setCommission(commission);
+                                                  // Actualizar saldo
+                                                  return customerProductRepository.update(update, e.getCustomerPassiveProductId())
+                                                        // Guardar operacion
+                                                        .flatMap(p -> Mono.just(e))
+                                                        .map(mapper::toEntity)
+                                                        .flatMap(repository::save)
+                                                        .map(mapper::toResponse)
+                                                        .switchIfEmpty(Mono.error(RuntimeException::new));
                                                 })
                                 )
                 );
